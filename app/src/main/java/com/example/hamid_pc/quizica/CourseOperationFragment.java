41d0200b252675812133b867ffcab74144af5359
Hamid-PC <jawadsom@yahoo.com>
2017-03-19 20:56:36 +0500

    Deleting Courses

--- app/src/main/java/com/example/hamid_pc/quizica/CourseOperationFragment.java
+++ app/src/main/java/com/example/hamid_pc/quizica/CourseOperationFragment.java
@@ -7,7 +7,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by Hamid-PC on 2/17/2017.
@@ -15,6 +23,9 @@ import android.widget.Toast;
 
 public class CourseOperationFragment extends DialogFragment {
 
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mCourseReference;
 
     public static CourseOperationFragment newInstance(String CourseName) {
         CourseOperationFragment frag = new CourseOperationFragment();
@@ -28,7 +39,8 @@ public class CourseOperationFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final String CourseName = getArguments().getString("coursename");
-
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mCourseReference = mFirebaseDatabase.getReference("course");
         return new AlertDialog.Builder(getActivity())
                 //        .setView(v)
                 .setTitle(R.string.operation_picker)
@@ -44,6 +56,20 @@ public class CourseOperationFragment extends DialogFragment {
                                 startActivity(intent);
                                 break;
                             case 2:
+                                Query query = mCourseReference.orderByChild("courseName").equalTo(CourseName);
+                                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        for (DataSnapshot coursSnapshot : dataSnapshot.getChildren()) {
+                                            coursSnapshot.getRef().removeValue();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(DatabaseError databaseError) {
+                                        Log.e("check", "onCancelled", databaseError.toException());
+                                    }
+                                });
                                 Toast.makeText(getActivity(), "You have pressed Update Quiz", Toast.LENGTH_LONG).show();
                                 break;
                         }
